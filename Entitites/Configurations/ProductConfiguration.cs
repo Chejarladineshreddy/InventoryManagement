087087801d3ae393de2d1247a440b2b71d04760e
Chejarladineshreddy <108918050+Chejarladineshreddy@users.noreply.github.com>
2026-03-17 17:26:42 +0530

    removing

---
+++ Entitites/Configurations/ProductConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Entitites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Entities.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Id).UseIdentityColumn().IsRequired();
+
+            builder.HasOne(p => p.category).WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId).OnDelete(DeleteBehavior.Cascade);
+
+
+
+        }
+    }
+}
---
+++ InventoryManagement/Controllers/CategoryCrudController.cs
@@ -0,0 +1,54 @@
+﻿
+using Entitites;
+//using ModalClass;
+using ModalClasses;
+using InventoryManagement.DatabaseActions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagement.Controllers
+{
+    [ApiController]
+    [Route("/Category")]
+    public class CategoryDb : Controller
+    {
+        private Categorydb _categoryDb;
+
+        public CategoryDb(Categorydb categoryDb)
+        {
+            _categoryDb = categoryDb;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategoriesAsync()
+        {
+            return Json(await _categoryDb.ViewAllCategoriesAsync());
+        }
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> GetCategoryAsync(String name)
+        {
+            return Json(await _categoryDb.GetProductByCategoryAsync(name));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostCategoryAsync([FromForm] CategoryDto categoryDto)
+        {
+            await _categoryDb.AddCategoryAsync(categoryDto);
+            return Json(await _categoryDb.ViewAllCategoriesAsync());
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCategoryAsync([FromForm] CategoryDto categoryDto)
+        {
+            await _categoryDb.DeleteCategoryAsync(categoryDto);
+            return Json(await _categoryDb.ViewAllCategoriesAsync());
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> UpdateCategoryAsync([FromBody] CategoryDto categoryDto)
+        {
+            await _categoryDb.UpdateCategoryAsync(categoryDto, ModelState);
+            return Json(await _categoryDb.ViewAllCategoriesAsync());
+        }
+    }
+}
